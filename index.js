1737e3b9795fa108d435e3b0c8f6fa42dc151ffc
MDrevon <MsDrevon@gmail.com>
2023-03-14 16:06:41 -0500

    added some jquery to test creating elements with Bulma

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ $(document).ready(function () {
   $("#about").on("click", function () {
     alert("About was clicked");
   });
+  $("#next").on("click", function () {
+    //alert("Next was clicked");
+    createTile();
+  });
 });
+
+function createTile() {
+  let $div = $(`<div class="tile"></div`);
+
+  for (let x = 0; x < 3; x++) {
+    let $col = $(`<div class="tile">Col ${x}</div>`);
+    $div.append($col);
+  }
+  $body.append($div);
+}
